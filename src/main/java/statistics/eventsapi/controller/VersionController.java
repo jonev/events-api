4a8f38c21dcfb2defc58f5d49798fb1135033701
Jone Vassbø <jone.vassbo@gmail.com>
2020-12-19 18:16:06 +0100

    Added version path

---
+++ src/main/java/statistics/eventsapi/controller/VersionController.java
@@ -0,0 +1,22 @@
+package statistics.eventsapi.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+@RestController
+@RequestMapping("version")
+public class VersionController {
+
+    @Value("${version}")
+    private String version;
+
+    @GetMapping(produces = MediaType.TEXT_PLAIN_VALUE)
+    public String getVersion() {
+        return version;
+    }
+}
--- src/main/java/statistics/eventsapi/service/ModelConverter.java
+++ src/main/java/statistics/eventsapi/service/ModelConverter.java
@@ -1,7 +1,7 @@
 package statistics.eventsapi.service;
 
 public class ModelConverter {
-    public static Event convert(statistics.eventsapi.dto.Event event){
+    public static Event convert(statistics.eventsapi.dto.Event event) {
         return new Event(event.getUserId(),
                 event.getEventType(),
                 event.getAppId(),
